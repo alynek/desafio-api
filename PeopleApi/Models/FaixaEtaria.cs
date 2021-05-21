8c5d8b9e41d5e58b3e21b9cc8dfa7f20000cb355
Alyne Gois <alyne0x67@gmail.com>
2021-05-21 19:43:17 -0300

    Commit inicial

---
+++ PeopleApi/Models/FaixaEtaria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleApi.Models
+{
+    public class FaixaEtaria
+    {
+        public string Faixa1 { get; private set; } = "0-9";
+        public string Faixa2 { get; private set; } = "0-9";
+        public string Faixa3 { get; private set; } = "0-9";
+        public string Faixa4 { get; private set; } = "0-9";
+        public string Faixa5 { get; private set; } = "0-9";
+        public string Faixa6 { get; private set; } = "0-9";
+        public string Faixa7 { get; private set; } = "0-9";
+        public string Faixa8 { get; private set; } = "0-9";
+        public string Faixa9 { get; private set; } = "0-9";
+        public string Faixa10 { get; private set; } = "0-9";
+        public string Faixa11 { get; private set; } = "0-9";
+        public string Faixa12 { get; private set; } = "0-9";
+        public string Faixa13 { get; private set; } = "0-9";
+    }
+}
---
+++ PeopleApi/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PeopleApi.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+
+        public int MostrarIdade()
+        {
+            int anoAtual = DateTime.Now.Year;
+            return anoAtual - this.DataNascimento.Year;
+        }
+    }
+}
