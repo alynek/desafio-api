d079268d47a2a44b94b7e28b10455816cb518179
Alyne Gois <alyne0x67@gmail.com>
2021-05-22 13:02:32 -0300

    Adiciona nova migration para atualizar o valor máximo do campo Faixa no banco

--- PeopleApi/Migrations/20210522160047_AlteraTamanhoCamposFaixaEtaria.Designer.cs
+++ PeopleApi/Migrations/20210522160047_AlteraTamanhoCamposFaixaEtaria.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PeopleApi.Data;
 
 namespace PeopleApi.Migrations
 {
     [DbContext(typeof(PessoaContexto))]
-    partial class PessoaContextoModelSnapshot : ModelSnapshot
+    [Migration("20210522160047_AlteraTamanhoCamposFaixaEtaria")]
+    partial class AlteraTamanhoCamposFaixaEtaria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PeopleApi/Migrations/20210522160047_AlteraTamanhoCamposFaixaEtaria.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PeopleApi.Migrations
+{
+    public partial class AlteraTamanhoCamposFaixaEtaria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Faixa",
+                table: "FaixasEtarias",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Faixa",
+                table: "FaixasEtarias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
