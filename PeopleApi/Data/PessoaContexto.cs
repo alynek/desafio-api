b2932c77d21830afa766e8e0af369143d084f9de
Alyne Gois <alyne0x67@gmail.com>
2021-05-22 09:53:42 -0300

    Adiciona classe do DbContext

---
+++ PeopleApi/Data/PessoaContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleApi.Models;
+
+namespace PeopleApi.Data
+{
+    public class PessoaContexto : DbContext
+    {
+        public PessoaContexto(DbContextOptions<PessoaContexto> options) : base(options){}
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public DbSet<FaixaEtaria> FaixasEtarias { get; set; }
+    }
+}
