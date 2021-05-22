a8d30a1d71f9720bbd11ef08078f107bcde2cd10
Alyne Gois <alyne0x67@gmail.com>
2021-05-22 13:01:57 -0300

    Adiciona tamanho máximo do campo Faixa

--- PeopleApi/Models/FaixaEtaria.cs
+++ PeopleApi/Models/FaixaEtaria.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PeopleApi.Models
 {
     public class FaixaEtaria
     {
         public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Faixa { get; private set; } 
         public List<Pessoa> Pessoas { get; set; }
     }
