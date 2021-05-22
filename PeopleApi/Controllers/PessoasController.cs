bfb85cb02d6a64fd3d5291f506bb74dcad6d1dd4
Alyne Gois <alyne0x67@gmail.com>
2021-05-22 11:11:36 -0300

    Adiciona métodos crud na classe PessoaService

---
+++ PeopleApi/Controllers/PessoasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleApi.Data;
+using PeopleApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PeopleApi.Controllers
+{
+    [Route("api/pessoas")]
+    [ApiController]
+    public class PessoasController : ControllerBase
+    {
+        private readonly PessoaContexto _pessoaContexto;
+        public PessoasController(PessoaContexto pessoaContexto)
+        {
+            _pessoaContexto = pessoaContexto;
+        }
+
+        [HttpGet]
+        public List<Pessoa> Get()
+        {
+            var pessoas =  _pessoaContexto.
+        }
+
+        [HttpGet("{id}")]
+        public Pessoa Get(int id)
+        {
+            var pessoa =  _pessoaContexto.Pessoas.Find(id);
+            return pessoa;
+        }
+
+        [HttpPost]
+        public void Post(Pessoa pessoa)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Pessoa pessoa)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PeopleApi/Services/PessoaService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleApi.Data;
+using PeopleApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PeopleApi.Services
+{
+    public class PessoaService
+    {
+        private readonly PessoaContexto _contexto;
+        public PessoaService(PessoaContexto pessoaContexto)
+        {
+            _contexto = pessoaContexto;
+        }
+
+        public async Task<List<Pessoa>> ObterTodos()
+        {
+            return await _contexto.Pessoas.ToListAsync();
+        }
+
+        public async Task<Pessoa> ObterPorId(int id)
+        {
+            return await _contexto.Pessoas
+                .Include(x => x.FaixaEtaria)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Adicionar(Pessoa pessoa)
+        {
+            _contexto.Add(pessoa);
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task<Pessoa> Remover(int id)
+        {
+            Pessoa objetoEditado = await _contexto.Pessoas.FindAsync(id);
+
+            objetoEditado.Ativo = false;
+            _contexto.Update(objetoEditado);
+
+            return objetoEditado;
+        }
+
+        public async Task Atualizar(Pessoa pessoa)
+        {
+            bool pessoaExiste = await _contexto.Pessoas.AnyAsync(x => x.Id == pessoa.Id);
+
+            if (!pessoaExiste) throw new KeyNotFoundException("Pessoa não encontrada!");
+
+            try
+            {
+                _contexto.Update(pessoa);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException erro)
+            {
+                throw new DbUpdateConcurrencyException(erro.Message);
+            }
+        }
+    }
+}
