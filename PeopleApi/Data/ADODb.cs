4307523580ea6a12ec221ff1e6f352e5155acb5b
Alyne Gois <alyne0x67@gmail.com>
2021-05-23 15:50:21 -0300

    Cria classe que irá acessar os dados para para atualizar e remover, usando ADO.NET

---
+++ PeopleApi/Data/ADODb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace PeopleApi.Data
+{
+    public class ADODb
+    {
+        public SqlConnection connection;
+        public ADODb()
+        {
+            try
+            {
+                connection = new SqlConnection("Data Source=DESKTOP-OE806TC\\SQLEXPRESS;Initial Catalog=PeopleDb;Integrated Security=true");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void ExecutaQuery<T>(string queryString)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                command.CommandTimeout = 9600;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                reader.Close();
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
